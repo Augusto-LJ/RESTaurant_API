9e7f36200232aa8b1c347aab9fc724d01308c9dd
Augusto-LJ <augustolimajardim@gmail.com>
2025-12-13 18:09:23 -0300

    feat: creates GetMenuItems endpoint

---
+++ Controllers/MenuItemController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RESTaurant_API.Data;
+using RESTaurant_API.Models;
+using System.Net;
+
+namespace RESTaurant_API.Controllers
+{
+    [Route("api/MenuItem")]
+    [ApiController]
+    public class MenuItemController(ApplicationDbContext db) : Controller
+    {
+        private readonly ApplicationDbContext _db = db;
+        private readonly ApiResponse _response = new();
+
+        [HttpGet]
+        public IActionResult GetMenuItems()
+        {
+            _response.Result = _db.MenuItems.ToList();
+            _response.StatusCode = HttpStatusCode.OK;
+            return Ok(_response);
+        }
+    }
+}
--- Models/ApiResponse.cs
+++ Models/ApiResponse.cs
@@ -5,7 +5,7 @@ namespace RESTaurant_API.Models
     public class ApiResponse
     {
         public HttpStatusCode StatusCode { get; set; }
-        public bool IsSuccess { get; set; }
+        public bool IsSuccess { get; set; } = true;
         public List<string> ErrorMessages { get; set; } = [];
         public object? Result { get; set; }
     }
