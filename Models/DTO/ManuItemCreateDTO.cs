42cdf5b91c267a7352cda38a59cad04d08e66dfa
Augusto-LJ <augustolimajardim@gmail.com>
2026-01-11 21:05:25 -0300

    feat: Creates DTOs for menu item and updates MenuItem

---
+++ Models/DTO/ManuItemCreateDTO.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RESTaurant_API.Models.DTO;
+
+public class ManuItemCreateDTO
+{
+    [Required]
+    public string Name { get; set; } = string.Empty;
+    public string? Description { get; set; }
+    public string Category { get; set; } = string.Empty;
+    public string? SpecialTag { get; set; }
+    [Range(1, 1000)]
+    public double Price { get; set; }
+    [Required]
+    public IFormFile File { get; set; } = null!;
+}
